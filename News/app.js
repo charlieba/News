56f1e7dc22e834769aad50ea0b9a3930a2db3a00
giovanib02 <giovanib07@gmail.com>
2016-01-18 22:34:13 -0600

    create server and update file

---
+++ News/app.js
@@ -0,0 +1,15 @@
+﻿var http = require('http');
+var port = process.env.port || 3021;
+http.createServer(function (req, res) {
+    if (req.method == 'GET') {
+        var body = '';
+        var parametros = req.url.split('?');
+        switch (parametros[0]) {
+            case '/update_news':{ 
+                console.log('hello world');    
+            }
+        }
+    }
+    res.writeHead(404, "Not found", { 'Content-Type': 'text/html' });
+    res.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
+}).listen(80);
